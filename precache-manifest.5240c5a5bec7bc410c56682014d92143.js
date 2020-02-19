30154079aa96a7127941f376af2678fa012892ef
Shane O Donovan <kenshii.sod@gmail.com>
2020-02-19 01:01:08 +0000

    Updates

---
+++ precache-manifest.5240c5a5bec7bc410c56682014d92143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8ba575d1d70e2fe7c6b0",
+    "url": "/static/js/main.8ba575d1.chunk.js"
+  },
+  {
+    "revision": "9895d3e17d356e322675",
+    "url": "/static/js/1.9895d3e1.chunk.js"
+  },
+  {
+    "revision": "8ba575d1d70e2fe7c6b0",
+    "url": "/static/css/main.bdd52787.chunk.css"
+  },
+  {
+    "revision": "28a2c918e0804c804dbce40b1e164d98",
+    "url": "/index.html"
+  }
+];
